a50ef7eb613143e384118b86974aac28041bb6f1
Jesus Medina <medinabotamail@gmail.com>
2021-10-12 22:37:08 -0500

    flutter zoom drawer added

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
 import '../widgets/home_screen_end_drawer.dart';
 import '../widgets/home_screen_header_elements.dart';
 import '../models/user.dart';
@@ -21,6 +22,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin{
   HomeScreenProvider homeScreenProvider = HomeScreenProvider();
+  final _drawerController = ZoomDrawerController();
+  bool isDrawerOpen = false;
   List<Section>      sections           = [];
   late User user;
   @override
@@ -41,45 +44,63 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin{
             padding: const EdgeInsets.only(right: 15.0),
             child: Builder(
               builder: (context) => IconButton(
-                icon: const Icon(Icons.menu), 
+                icon: isDrawerOpen ? Icon(Icons.close) : Icon(Icons.menu), 
                 color: Theme.of(context).iconTheme.color,
                 iconSize: Theme.of(context).iconTheme.size!,
-                onPressed: (){ Scaffold.of(context).openEndDrawer(); }
+                onPressed: (){ 
+                    // Scaffold.of(context).openEndDrawer();
+                    if (_drawerController.stateNotifier!.value == DrawerState.closed) {
+                      isDrawerOpen = true;
+                      _drawerController.open!();
+                    } else if (_drawerController.stateNotifier!.value == DrawerState.open) {
+                      isDrawerOpen = false;
+                      _drawerController.close!();
+                    }
+                    setState(() {
+                      
+                    });
+                  }
                 ),
             ),
           )
         ]
       ),
-      endDrawer: Drawer(
+      endDrawer: const Drawer(
         child: EndDrawer(),
       ),
-      body: Stack(
-        children: <Widget>[
-          const HomeScreenBackground(),
-          HomeScreenHeader(
-            child: Container(
-              // padding: const EdgeInsets.only(top: 70.0),
-              width: 325.0,
-              height: 370.0,
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-                borderRadius: const BorderRadius.only(bottomRight: Radius.circular(70)),
-                boxShadow: [
-                  BoxShadow(
-                      color: Theme.of(context).primaryColor.withOpacity(0.3),
-                      spreadRadius: 5,
-                      blurRadius: 10,
-                      offset: const Offset(0, 2)),
-                ],
+      body: ZoomDrawer(
+        controller: _drawerController,
+        menuScreen: EndDrawer(),
+        mainScreen: Stack(
+          children: <Widget>[
+            const HomeScreenBackground(),
+            HomeScreenHeader(
+              child: Container(
+                // padding: const EdgeInsets.only(top: 70.0),
+                width: 325.0,
+                height: 370.0,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: const BorderRadius.only(bottomRight: Radius.circular(70)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Theme.of(context).primaryColor.withOpacity(0.3),
+                        spreadRadius: 5,
+                        blurRadius: 10,
+                        offset: const Offset(0, 2)),
+                  ],
+                ),
+                child: HomeScreenHeaderElements(user: user),
               ),
-              child: HomeScreenHeaderElements(user: user),
             ),
-          ),
-          HomeScreenSection(
-            sections: sections,
+            HomeScreenSection(
+              sections: sections,
+              // onTap: 
             // onTap: 
-          )
-        ],
+              // onTap: 
+            )
+          ],
+        ),
       )
     );
   }
--- lib/src/widgets/home_screen_end_drawer.dart
+++ lib/src/widgets/home_screen_end_drawer.dart
@@ -13,20 +13,20 @@ class _EndDrawerState extends State<EndDrawer> {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        actions: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: 15.0),
-            child: Builder(
-              builder: (context) => IconButton(
-                icon: const Icon(Icons.close), 
-                color: Theme.of(context).iconTheme.color,
-                iconSize: Theme.of(context).iconTheme.size!,
-                onPressed: (){ Navigator.of(context).pop(); }
-                ),
-            ),
-          )
-        ],
-        centerTitle: true,
+        // actions: <Widget>[
+        //   Padding(
+        //     padding: const EdgeInsets.only(right: 15.0),
+        //     child: Builder(
+        //       builder: (context) => IconButton(
+        //         icon: const Icon(Icons.close), 
+        //         color: Theme.of(context).iconTheme.color,
+        //         iconSize: Theme.of(context).iconTheme.size!,
+        //         onPressed: (){ Navigator.of(context).pop(); }
+        //         ),
+        //     ),
+        //   )
+        // ],
+        centerTitle: false,
         title: Text('Settings', style: Theme.of(context).textTheme.headline6,),
       )
     );
